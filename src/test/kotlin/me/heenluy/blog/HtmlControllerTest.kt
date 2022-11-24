27f25e5c45912699a6e6ab242150b899ed1e77bc
Henrique Luiz <heenluy@gmail.com>
2022-11-24 14:52:28 -0300

    test: testing 'HtmlController' with TestRestTemplate

---
+++ src/test/kotlin/me/heenluy/blog/HtmlControllerTest.kt
@@ -0,0 +1,31 @@
+package me.heenluy.blog
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class HtmlControllerTest(@Autowired val restTemplate: TestRestTemplate) {
+
+    @Test
+    fun `Assert blog page title, content and status code`() {
+        val entity = restTemplate.getForEntity<String>("/")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>Blog</h1>")
+    }
+
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> Assert article page title, content and status code")
+        val title = "Reactor Aluminium has landed"
+        val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains(title, "Lorem ipsum", "dolor sit amet")
+    }
+
+}
